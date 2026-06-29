6ec7d2a7d5dc735b27529e0b95aa0759493c67e1
vikash kumar <vk0812155@gmail.com>
2026-06-30 04:22:45 +0530

    Update script.js
    update image at tem >35

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ function changeBackground(temp, condition){
     else if(temp >= 35){
 
         document.body.style.backgroundImage =
-        'linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3)),url("safari.jpeg")';
+        'linear-gradient(rgba(0,0,0,.3),rgba(0,0,0,.3)),url("hotest.jfif")';
 
     }
 
